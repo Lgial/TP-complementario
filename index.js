cb63754e8dd8e7b9e3ab7b9dcd472eea3485d4ca
unknown <leandrogiardelli@hotmail.com>
2022-06-18 17:45:23 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,25 @@
+/*let n1 = prompt("Escribe un número")
+let n2 = prompt("Escribe otro número")
+if (n1 > n2) {
+alert("n1");
+} else {
+alert("n2");
+} while(entrada !=="ESC") {
+    alert("El usuario ingreso: " + entrada)
+    entrada = prompt("Ingresa otro dato")
+}*/
+
+
+let n1 = prompt("Escribe un número(ESC para salir)")
+let n2 = prompt("Escribe otro número(ESC para salir)")
+let entrada
+while(entrada !="ESC") {
+    if (n1 > n2) {
+        alert("n1");
+        } else {
+        alert("n2");
+ //   alert("El usuario ingreso: "+ entrada)
+  //  entrada = prompt("Ingresa otro dato")
+}
+}
+while(n1)
