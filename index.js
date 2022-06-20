bcc24d5babff9ae96d8ca9332c6cf8c7c2a5dacc
unknown <leandrogiardelli@hotmail.com>
2022-06-20 10:21:59 -0300

    Trabajo cambiado y terminado

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-/*let n1 = prompt("Escribe un número")
-let n2 = prompt("Escribe otro número")
-if (n1 > n2) {
-alert("n1");
-} else {
-alert("n2");
-} while(entrada !=="ESC") {
-    alert("El usuario ingreso: " + entrada)
-    entrada = prompt("Ingresa otro dato")
-}*/
+let nota = 0
+let prom = 0
+let cant = 0
 
+do {
+    cant++;
+    prom+=parseFloat(nota)
+    nota =parseFloat(prompt("Ingrese una nota(ESC para finalizar)", ""))
 
-let n1 = prompt("Escribe un número(ESC para salir)")
-let n2 = prompt("Escribe otro número(ESC para salir)")
-let entrada
-while(entrada !="ESC") {
-    if (n1 > n2) {
-        alert("n1");
-        } else {
-        alert("n2");
- //   alert("El usuario ingreso: "+ entrada)
-  //  entrada = prompt("Ingresa otro dato")
-}
-}
-while(n1)
+} while(nota != "ESC")
+alert("El promedio es de "+ prom/cant)
