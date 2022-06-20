a3a2e17f96634a0a42d891192d9ba0a099698208
unknown <leandrogiardelli@hotmail.com>
2022-06-20 10:32:13 -0300

    Error arreglado, tp terminado

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let cant = 0
 do {
     cant++;
     prom+=parseFloat(nota)
-    nota =parseFloat(prompt("Ingrese una nota(ESC para finalizar)", ""))
+    nota =parseFloat(prompt("Ingrese una nota(-1 para ver la nota)", ""))
 
-} while(nota != "ESC")
+} while(nota != "-1")
 alert("El promedio es de "+ prom/cant)
